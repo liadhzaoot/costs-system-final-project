27dc116d5602e5d81a5d17fd2dfcd29f6e3f9acf
Dvir Yom Tov <dvirmb@ip-192-168-1-218.ec2.internal>
2022-06-21 01:41:28 +0300

    all user api works

--- routes/user.js
+++ routes/user.js
@@ -16,4 +16,47 @@ router.post('/user',(req,res)=>{
         )
     }
 )
+
+router.get("/user/:id", (req, res) => {
+    const user = User.findOne({ _id: req.params.id }).then((result) => res.send(result))
+})
+
+router.get("/user", (req, res) => {
+    const users = User.find().then((result) => res.send(result))
+})
+
+router.patch("/user/:id",  (req, res) => {
+    try {
+        const post = User.findOne({ _id: req.params.id }).then((result)=>{
+            if (req.body.first_name) {
+                result.first_name = req.body.first_name
+            }
+            if(req.body.last_name){
+                result.last_name = req.body.last_name
+            }
+            if(req.body.birthday)
+            {
+                result.birthday = req.body.birthday
+            }
+            if(req.body.marital_status)
+            {
+                result.marital_status = req.body.marital_status
+            }
+            result.save().then((result)=> res.send(result))
+        })
+
+    } catch {
+        res.status(404)
+        res.send({ error: "Post doesn't exist!" })
+    }
+})
+router.delete("/user/:id",  (req, res) => {
+    try {
+        User.deleteOne({ _id: req.params.id }).then((result) => res.status(204).send())
+    } catch {
+        res.status(404)
+        res.send({ error: "Post doesn't exist!"})
+    }
+})
+
 module.exports = router
